f3bbc9563d130136191dc245de4e65110abf4be4
WillSunders <88588189+WillSunders@users.noreply.github.com>
2026-09-09 10:36:02 +0100

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        visited = set()
+        connect = defaultdict(list)
+        for v1, v2 in edges:
+            connect[v1].append(v2)
+            connect[v2].append(v1)
+        
+        q = deque([(0, -1)])
+        visited.add(0)
+        while q:
+            v, prev = q.popleft()
+            for nb in connect[v]:
+                if nb != prev:
+                    if nb in visited:
+                        return False
+                    visited.add(nb)
+                    q.append((nb, v))
+
+        return len(visited) == n
+
+
+
+
+
